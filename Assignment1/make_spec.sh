929518e2c607aa92ed19875c586b95b10131683e
Arham Chopra <arhamschopra@gmail.com>
2018-01-16 19:38:18 +0530

    Added Assignment1 with spec zip and problem statement

---
+++ Assignment1/make_spec.sh
@@ -0,0 +1,7 @@
+# Instructions to make spec_2006 from the zip files
+mkdir spec_2006
+mv spec2006_1.zip spec_2006
+mv spec2006_2.zip spec_2006
+cd spec_2006
+unzip spec2006_1.zip
+unzip spec2006_2.zip
